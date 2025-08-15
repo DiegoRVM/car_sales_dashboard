c5ecf316b3251681e8d58ff2d3eb4e05e3045d55
Diego Raniel Vaz Mendes <diego.rvm@gmail.com>
2025-08-15 13:35:43 -0300

    atualizações

--- app.py
+++ app.py
@@ -2,41 +2,43 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Título do dashboard
 st.header('Dashboard de anúncios de carros')
 
-# Carregar os dados
-# O Streamlit recomenda o uso de st.cache para carregar dados pesados apenas uma vez.
 @st.cache_data
 def load_data():
     return pd.read_csv('vehicles.csv')
 
 car_data = load_data()
 
-# Adicionar a barra lateral para opções de gráficos
 st.sidebar.header('Opções de gráficos')
 
-# Criar as caixas de seleção na barra lateral
 hist_checkbox = st.sidebar.checkbox('Criar histograma de quilometragem')
 scatter_checkbox = st.sidebar.checkbox('Criar gráfico de dispersão de preço x quilometragem')
+bar_chart_checkbox = st.sidebar.checkbox('Criar gráfico de barras de preço por condição')
 
-# Lógica para as caixas de seleção
 if hist_checkbox:
     st.subheader('Distribuição de Quilometragem')
     st.write('Criando um histograma para a coluna de quilometragem...')
-    
-    # Criar o histograma
+        
     fig_hist = px.histogram(car_data, x="odometer")
-    
-    # Exibir o gráfico Plotly
+        
     st.plotly_chart(fig_hist, use_container_width=True)
 
 if scatter_checkbox:
     st.subheader('Preço vs. Quilometragem')
     st.write('Criando um gráfico de dispersão entre preço e quilometragem...')
     
-    # Criar o gráfico de dispersão
     fig_scatter = px.scatter(car_data, x="odometer", y="price")
-    
-    # Exibir o gráfico Plotly
+        
     st.plotly_chart(fig_scatter, use_container_width=True)
+
+if bar_chart_checkbox:
+    st.subheader('Preço médio por Condição do Veículo')
+    st.write('Criando um gráfico de barras de preço por condição...')
+        
+    avg_price_by_condition = car_data.groupby('condition')['price'].mean().reset_index()
+        
+    fig_bar = px.bar(avg_price_by_condition, x='condition', y='price', 
+                     title='Preço Médio por Condição do Veículo')
+        
+    st.plotly_chart(fig_bar, use_container_width=True)
